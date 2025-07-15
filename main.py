8ab5b3205b69a653585b447c3e58b465ec24e8b3
rockefelm <rockefelm@gmail.com>
2025-07-15 12:26:51 -0400

    added ability to use verbose flag and set the prompt input

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-api_key = os.getenv("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 def main():
-    print("Hello from aiagent!")
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <prompt_string>")
+        sys.exit(1)
+    prompt_string = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt_string)]),
+    ]
+    print(f"Prompt string: {prompt_string}")
+    api_key = os.getenv("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages
+    )
+    print(response.text)
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {prompt_string}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 
 if __name__ == "__main__":
