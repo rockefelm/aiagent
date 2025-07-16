94edb446fe28cd7aa5933398e282b632a2bd9c6d
rockefelm <rockefelm@gmail.com>
2025-07-16 11:58:44 -0400

    cretaed the run_python function and test cases (WARNING: this program is for learning purposes only run at your own risk.)

---
+++ functions/run_python.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=[]):
+    joined_path = os.path.join(working_directory, file_path)
+    absolute_path = os.path.abspath(joined_path)
+    
+    if not absolute_path.startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(absolute_path):
+        return f'Error: File "{file_path}" not found.'
+    if not file_path.endswith('.py'):
+        return f'Error: "{file_path}" is not a Python file.'
+    try:
+        completed_process = subprocess.run(
+            ['python', absolute_path] + args,
+            check=True,
+            timeout=30,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            cwd=working_directory
+        )
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+    if (not completed_process.stdout.strip()) and (not completed_process.stderr.strip()):
+        return "No output produced."
+
+    output_parts = []
+    if completed_process.stdout.strip():
+        output_parts.append("STDOUT: " + completed_process.stdout)
+    if completed_process.stderr.strip():
+        output_parts.append("STDERR: " + completed_process.stderr)
+    if completed_process.returncode != 0:
+        output_parts.append(f"Process exited with code {completed_process.returncode}")
+    return "\n".join(output_parts)
