e4400db30124105eb1f6c3fc643348bcd5e5f81e
rockefelm <rockefelm@gmail.com>
2025-07-16 13:15:01 -0400

    created first schema to get the AI to run the get_files_info function in main.py

--- main.py
+++ main.py
@@ -3,13 +3,27 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-
+from functions.get_files_info import *
 
 
 def main():
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
     if len(sys.argv) < 2:
         print("Usage: python main.py <prompt_string>")
         sys.exit(1)
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
     prompt_string = sys.argv[1]
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt_string)]),
@@ -19,9 +33,19 @@ def main():
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
-    print(response.text)
+    if response.function_calls:
+        for function in response.function_calls:
+            if function.name == "get_files_info":
+                directory = function.args.get("directory", ".")
+                print(f"Calling function: {function.name}({function.args})")
+                print(get_files_info(os.getcwd(), directory))         
+    else:
+        print(response.text)
     if "--verbose" in sys.argv:
         print(f"User prompt: {prompt_string}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
