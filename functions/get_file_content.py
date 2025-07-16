384ba884b8bd7580048f5850500a340a6d53ee86
rockefelm <rockefelm@gmail.com>
2025-07-16 13:39:52 -0400

    completed the schema for run_python, write_file, and get_file_content.

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,6 @@
 import os
 from functions.config import *
+from google.genai import types
 
 def get_file_content(working_directory, file_path):
     joined_path = os.path.join(working_directory, file_path)
@@ -18,3 +19,18 @@ def get_file_content(working_directory, file_path):
         return content
     except Exception as e:
         return f'Error: {str(e)}'
+    
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Reads the content of a file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to read, relative to the working directory.",
+            ),
+        },
+    required=["file_path"],
+    ),
+)
--- functions/run_python.py
+++ functions/run_python.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from google.genai import types
 
 def run_python_file(working_directory, file_path, args=[]):
     joined_path = os.path.join(working_directory, file_path)
@@ -34,3 +35,25 @@ def run_python_file(working_directory, file_path, args=[]):
     if completed_process.returncode != 0:
         output_parts.append(f"Process exited with code {completed_process.returncode}")
     return "\n".join(output_parts)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python",
+    description="Runs a .py file constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the Python file to execute, relative to the working directory.",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                items=types.Schema(
+                    type=types.Type.STRING,
+                    description="Optional arguments to pass to the Python file.",
+                ),
+            ),
+        },
+        required=["file_path"],
+    ),
+)
